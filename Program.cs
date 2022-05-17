91efe269354a5769d3250c12b4242fadc5865106
Facundo Abbas <facundoabbas@hotmail.com>
2022-05-17 14:47:04 -0300

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace tp052022{
+
+    class Program{
+
+
+
+        static void Main(string[] args){
+
+            int numero = Convert.ToInt32(Console.ReadLine());
+            string numInvertido = "";
+
+
+            foreach(char c in Convert.ToString(numero)){
+                numInvertido = c + numInvertido;
+            }
+
+            Console.WriteLine(numInvertido);
+
+
+
+        }
+
+    }
+
+}
