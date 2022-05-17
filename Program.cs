e21310fd419e6eb9738c1f0beca483483058a4f3
Facundo Abbas <facundoabbas@hotmail.com>
2022-05-17 16:41:19 -0300

    Punto 5

--- Program.cs
+++ Program.cs
@@ -7,21 +7,52 @@ namespace tp052022{
 
 
         static void Main(string[] args){
+            
 
-            int numero = Convert.ToInt32(Console.ReadLine());
-            string numInvertido = "";
-
-
-            foreach(char c in Convert.ToString(numero)){
-                numInvertido = c + numInvertido;
+            Console.WriteLine("Ingrese la cadena");
+            string palabra = Console.ReadLine();
+            string subpala = palabra.Substring(0,2);
+            string palabra2 ="Helado";
+            Console.WriteLine(subpala);
+            foreach (var item in palabra)
+            {
+               Console.WriteLine(item);
+            }
+            
+         
+          
+            
+            int longi = palabra.Length;
+            string conc = palabra+palabra2;
+            Console.WriteLine(palabra.ToUpper());
+            Console.WriteLine(palabra.ToLower());
+            if (palabra.Equals(palabra2))
+            {
+                Console.WriteLine("Son iguales");
+            }else
+            {
+                Console.WriteLine("NO son iguales");
+            }
+            string CorPal = "Hola . Mundo . Cortado . En . Partes";
+            string[] sub = CorPal.Split('.');
+            foreach (var item in sub)
+            {
+                Console.WriteLine($"SubsStrinfs: {item}");
             }
 
-            Console.WriteLine(numInvertido);
 
 
 
-        }
+           
+            Console.WriteLine(palabra);
+            Console.WriteLine(longi);
+            Console.WriteLine(conc);
+
+
+
+        
 
     }
 
+}
 }
